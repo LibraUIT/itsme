fd3a22e9c6107edac5154d8ca45c75c67894439f
Quan Nguyen <quanmn.libra@gmail.com>
2016-09-23 23:07:21 +0700

    Linking for questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917042054) do
+ActiveRecord::Schema.define(version: 20160923032055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,6 +144,13 @@ ActiveRecord::Schema.define(version: 20160917042054) do
   create_table "policies", force: :cascade do |t|
     t.text    "content"
     t.boolean "status",  default: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "content"
+    t.boolean  "status",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "ranks", force: :cascade do |t|
